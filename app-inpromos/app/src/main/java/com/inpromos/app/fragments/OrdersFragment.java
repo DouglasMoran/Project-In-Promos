30402d97d9a756f772a87774b395e8702dc5e4de
_vegeuwu <josueardon.developer@gmail.com>
2019-11-07 18:10:26 -0600

    Option selector bottom sheet

--- app-inpromos/app/src/main/java/com/inpromos/app/fragments/OrdersFragment.java
+++ app-inpromos/app/src/main/java/com/inpromos/app/fragments/OrdersFragment.java
@@ -9,10 +9,13 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.inpromos.app.R;
 import com.inpromos.app.adapters.OrderAdapter;
 import com.inpromos.app.models.OrderModel;
@@ -25,6 +28,7 @@ public class OrdersFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private OrderAdapter mAdapter;
     private List<OrderModel> orders = new ArrayList<>();
+    private FloatingActionButton mFab;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,14 +39,21 @@ public class OrdersFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        getReferences();
 
         generateTempData();
         setRecyclerView();
 
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheet();
+            }
+        });
+
     }
 
     private void setRecyclerView() {
-        mRecyclerView = getActivity().findViewById(R.id.ordersRecyclerView);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         mAdapter = new OrderAdapter(orders, getActivity());
         mRecyclerView.setAdapter(mAdapter);
@@ -50,8 +61,48 @@ public class OrdersFragment extends Fragment {
 
 
     private void generateTempData() {
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 5; i++) {
             orders.add(new OrderModel(i, "Number " + i, "Date " + i, i));
         }
     }
+
+    private void showBottomSheet() {
+        //Setting view
+        final BottomSheetDialog bottomSheet = new BottomSheetDialog(getActivity(), R.style.BottomSheetDialogStyle);
+        View sheetView = LayoutInflater.from(getActivity()).inflate(R.layout.sheet_selector, null, false);
+        bottomSheet.setContentView(sheetView);
+
+        bottomSheet.show();
+
+        //Listeners
+        sheetView.findViewById(R.id.sheetCompanyBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Debug", "Empresarial.");
+                bottomSheet.dismiss();
+            }
+        });
+
+        sheetView.findViewById(R.id.sheetCustomBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Debug", "Personalizado.");
+                bottomSheet.dismiss();
+            }
+        });
+
+        sheetView.findViewById(R.id.sheetCloseBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheet.dismiss();
+            }
+        });
+
+    }
+
+    private void getReferences() {
+        mFab = getActivity().findViewById(R.id.addOrderFab);
+        mRecyclerView = getActivity().findViewById(R.id.ordersRecyclerView);
+    }
+
 }
